252299d1b719e424361a5bfc8fd7316a74484522
Frederic Charette <fredericcharette@gmail.com>
2025-07-24 13:19:47 -0400

    removed validation from MCP plugin

--- packages/mcp/src/index.ts
+++ packages/mcp/src/index.ts
@@ -1,11 +1,7 @@
 import definition from './definition.ts';
-import response from './response.ts';
 import router from './router.ts';
-import validation from './validation.ts';
 
 export {
   definition,
-  response,
   router,
-  validation,
 };
